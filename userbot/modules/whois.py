9eef4e8c7a5bb5cf32652f6938f04ccd1902b2a9
jefa2231 <39442318+jefa2231@users.noreply.github.com>
2020-04-08 16:15:10 +0700

    FIX : EN => ID

--- userbot/modules/whois.py
+++ userbot/modules/whois.py
@@ -17,11 +17,11 @@ from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY
 from userbot.events import register
 
 
-@register(pattern=".whois(?: |$)(.*)", outgoing=True)
+@register(pattern=".siapa(?: |$)(.*)", outgoing=True)
 async def who(event):
 
     await event.edit(
-        "`Sit tight while I steal some data from *Global Network Zone*...`")
+        "`Sedang mencari informasi dari **Anak Gang Sebelah**...`")
 
     if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
         os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
@@ -31,7 +31,7 @@ async def who(event):
     try:
         photo, caption = await fetch_info(replied_user, event)
     except AttributeError:
-        return event.edit("`Could not fetch info of that user.`")
+        return event.edit("`Tidak dapat mengambil info dari pengguna itu, Karena di privasi .`")
 
     message_id_to_reply = event.message.reply_to_msg_id
 
@@ -96,7 +96,7 @@ async def fetch_info(replied_user, event):
                              offset=42,
                              max_id=0,
                              limit=80))
-    replied_user_profile_photos_count = "Person needs help with uploading profile picture."
+    replied_user_profile_photos_count = "Kamu perlu bantuan dalam mengunggah foto profil?."
     try:
         replied_user_profile_photos_count = replied_user_profile_photos.count
     except AttributeError:
@@ -120,33 +120,33 @@ async def fetch_info(replied_user, event):
                                                       str(user_id) + ".jpg",
                                                       download_big=True)
     first_name = first_name.replace(
-        "\u2060", "") if first_name else ("This User has no First Name")
+        "\u2060", "") if first_name else ("Pengguna ini tidak memiliki Nama Depan")
     last_name = last_name.replace(
-        "\u2060", "") if last_name else ("This User has no Last Name")
+        "\u2060", "") if last_name else ("Pengguna ini tidak memiliki Nama Belakang")
     username = "@{}".format(username) if username else (
-        "This User has no Username")
-    user_bio = "This User has no About" if not user_bio else user_bio
+        "Pengguna ini tidak memiliki Nama Pengguna")
+    user_bio = "Pengguna ini tidak memiliki BIO" if not user_bio else user_bio
 
-    caption = "<b>USER INFO:</b>\n\n"
-    caption += f"First Name: {first_name}\n"
-    caption += f"Last Name: {last_name}\n"
-    caption += f"Username: {username}\n"
-    caption += f"Data Centre ID: {dc_id}\n"
-    caption += f"Number of Profile Pics: {replied_user_profile_photos_count}\n"
-    caption += f"Is Bot: {is_bot}\n"
-    caption += f"Is Restricted: {restricted}\n"
-    caption += f"Is Verified by Telegram: {verified}\n"
+    caption = "<b>INFORMASI USER:</b>\n\n"
+    caption += f"Nama Depan: {first_name}\n"
+    caption += f"Nama Belakang: {last_name}\n"
+    caption += f"Nama Pengguna: {username}\n"
+    caption += f"ID Pusat Data: {dc_id}\n"
+    caption += f"Jumlah Foto Profil: {replied_user_profile_photos_count}\n"
+    caption += f"Dia bot: {is_bot}\n"
+    caption += f"Di Dibatasi: {restricted}\n"
+    caption += f"Diverifikasi oleh Telegram: {verified}\n"
     caption += f"ID: <code>{user_id}</code>\n\n"
     caption += f"Bio: \n<code>{user_bio}</code>\n\n"
-    caption += f"Common Chats with this user: {common_chat}\n"
-    caption += f"Permanent Link To Profile: "
+    caption += f"Obrolan umum dengan pengguna ini: {common_chat}\n"
+    caption += f"Link Profile Selamanya Milik Pengguna ini: "
     caption += f"<a href=\"tg://user?id={user_id}\">{first_name}</a>"
 
     return photo, caption
 
 
 CMD_HELP.update({
-    "whois":
-    ">`.whois <username> or reply to someones text with .whois`"
-    "\nUsage: Gets info of an user."
+    "siapa":
+    ">`.siapa <username> atau balas pesan seseorang dengan mengetik .siapa`"
+    "\nFungsi: Mendapatkan Info Pengguna Tersebut."
 })
