c80bd3d361ebefb995b513b71cb781968b09ec93
jefa2231 <39442318+jefa2231@users.noreply.github.com>
2020-04-08 16:16:37 +0700

    fix siapa.py

--- userbot/modules/siapa.py
+++ userbot/modules/siapa.py
@@ -21,7 +21,7 @@ from userbot.events import register
 async def who(event):
 
     await event.edit(
-        "`Sedang mencari informasi dari **Anak Gang Sebelah**...`")
+        "`Sedang mencari informasi dari` **Anak Gang Sebelah...**")
 
     if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
         os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
